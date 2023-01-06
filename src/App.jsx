65d4a5bea8806875c643f6c7c8c34ba3048cdbe5
sebahattin <sebkucuk05@gmail.com>
2023-01-07 02:00:53 +0300

    new

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,7 @@ function App() {
     setLoading(true)
     setTimeout(() => {
       setLoading(false)
-    }, 6000)
+    }, 3000)
   }, [])
   return (
     <div className="App">
@@ -39,10 +39,15 @@ function App() {
 
             size={150}
             aria-label="Loading Spinner"
-            data-testid="loader"
-          />
+            data-testid="loader" />
+
           :
-          <img src="https://scontent.fist1-2.fna.fbcdn.net/v/t39.30808-6/300605754_102816295892106_2650946019783402378_n.jpg?stp=dst-jpg_s640x640&_nc_cat=109&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=yhPUMwR8mR4AX_s21Kd&tn=GvDPzuB365ZNaPb7&_nc_ht=scontent.fist1-2.fna&oh=00_AfCl7sLsl3H6cW3nA8gMHNufu-VMaq8oyPgBYMe2cMKnkA&oe=63AF3AB5" alt="" />
+          // <img src="https://scontent.fist1-2.fna.fbcdn.net/v/t39.30808-6/300605754_102816295892106_2650946019783402378_n.jpg?stp=dst-jpg_s640x640&_nc_cat=109&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=yhPUMwR8mR4AX_s21Kd&tn=GvDPzuB365ZNaPb7&_nc_ht=scontent.fist1-2.fna&oh=00_AfCl7sLsl3H6cW3nA8gMHNufu-VMaq8oyPgBYMe2cMKnkA&oe=63AF3AB5" alt="" />
+          // <img src="https://scontent.fist1-2.fna.fbcdn.net/v/t39.30808-6/300605754_102816295892106_2650946019783402378_n.jpg?stp=dst-jpg_s640x640&_nc_cat=109&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=yhPUMwR8mR4AX_s21Kd&tn=GvDPzuB365ZNaPb7&_nc_ht=scontent.fist1-2.fna&oh=00_AfCl7sLsl3H6cW3nA8gMHNufu-VMaq8oyPgBYMe2cMKnkA&oe=63AF3AB5" alt="" />
+          <>
+            <iframe src="https://giphy.com/embed/2ikwIgNrmPZICNmRyX" width="480" height="480" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/2ikwIgNrmPZICNmRyX"></a></p></>
+        // <img src="https://c.tenor.com/32nHeCvxlwEAAAAM/you-tube-like-and-subscribe.gif" />
+
       }
       {/* <div className="usttekal">
         deneme usttekal
